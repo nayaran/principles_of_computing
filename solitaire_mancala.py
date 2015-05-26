f6f95f604bd6f94d810a694be3e8031d455d75b7
narayan <anuragallahabad@gmail.com>
2015-05-26 23:13:20 +0530

    fix- infinite loop in OwlTest because we are trying to win the game in boards that couldn't be won

--- solitaire_mancala.py
+++ solitaire_mancala.py
@@ -127,6 +127,8 @@ class SolitaireMancala:
         # print "move chosen - ", min(legal_moves)
         if legal_moves:
             return min(legal_moves)
+        else:
+            return 0
     
     def plan_moves(self):
         """
@@ -147,6 +149,9 @@ class SolitaireMancala:
             current_move = self.choose_move()
             # print
             # print "current_move - ", current_move
+            if not current_move:
+                return ideal_moves
+            
             self.apply_move(current_move)
             ideal_moves.append(current_move)
 
@@ -167,21 +172,21 @@ def test_mancala():
     # print "Testing init - Computed:", my_game, "Expected: [0]"
     
     config1 = [0, 0, 1, 1, 3, 5, 0] 
-   # config1 = [0, 0, 0, 0]
+    #config1 = [0, 0, 0, 0]
     my_game.set_board(config1)   
     
 #    # print "Testing set_board - Computed:", str(my_game), "Expected:", str([0, 5, 3, 1, 1, 0, 0])
 #    # print "Testing get_num_seeds - Computed:", my_game.get_num_seeds(1), "Expected:", config1[1]
 #    # print "Testing get_num_seeds - Computed:", my_game.get_num_seeds(3), "Expected:", config1[3]
 #    # print "Testing get_num_seeds - Computed:", my_game.get_num_seeds(5), "Expected:", config1[5]
-    print
+
     print "playing the game....."
     my_game.plan_moves()
     my_game.is_game_won()
     
     # add more tests here
     
-test_mancala()
+#test_mancala()
 
 
 # Import GUI code once you feel your code is correct
