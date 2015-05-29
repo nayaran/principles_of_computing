a8077eb04e6a546f4d9c4c3e1e924b06547ae0e8
narayan <anuragallahabad@gmail.com>
2015-05-29 08:13:30 +0530

    2048merge- game works!

--- 2048/2048merge.py
+++ 2048/2048merge.py
@@ -6,5 +6,54 @@ def merge(line):
     """
     Function that merges a single row or column in 2048.
     """
-    # replace with your code
-    return []
+
+    # creates a copy of the line
+
+    row = list(line)
+    result = [0] * len(line)
+
+    # result line
+    destIndex = 0
+
+    # flag to make sure that we merge a tile only once
+    canMerge = True
+
+    # iterate over the given row and
+    # if found non_zero value,
+    # either merge it with the previous
+    # value in the result or put it as it is
+    #
+    for sourceIndex in range(len(row)):
+
+        if row[sourceIndex]:
+
+            # if non_zero value found
+            if result[destIndex-1] == row[sourceIndex] and canMerge:
+                # merge can be done
+                # hence merge
+                result[destIndex-1] *= 2
+
+                # disable merging for the merged tile
+                canMerge = False
+
+            else:
+
+                # no merging can be done
+                # hence copy the value as it is
+                result[destIndex] = row[sourceIndex]
+                destIndex += 1
+
+                # enable merging
+                canMerge = True
+
+
+    return result
+
+
+
+print "[2, 0, 2, 4] should return [4, 4, 0, 0], returns - ", merge([2, 0, 2, 4])
+print "[0, 0, 2, 2] should return [4, 0, 0, 0], returns - ", merge([0, 0, 2, 2])
+print "[2, 2, 0, 0] should return [4, 0, 0, 0], returns - ", merge([2, 2, 0, 0])
+print "[2, 2, 2, 2, 2] should return [4, 4, 2, 0, 0], returns - ", merge([2, 2, 2, 2, 2])
+print "[8, 16, 16, 8] should return [8, 32, 8, 0], returns - ", merge([8, 16, 16, 8])
+print "[2, 0, 2, 4, 0, 4, 8, 8] should return [4, 8, 16, 0, 0, 0, 0, 0] returns - ", merge([2, 0, 2, 4, 0, 4, 8, 8])
--- solitaire_mancala/solitaire_mancala.py
+++ solitaire_mancala/solitaire_mancala.py
@@ -11,14 +11,14 @@ class SolitaireMancala:
     """
     Simple class that implements Solitaire Mancala
     """
-    
+
     def __init__(self):
         """
         Create Mancala game with empty store and no houses
         """
         self.__config = [0]
 
-    
+
     def set_board(self, __config):
         """
         Take the list __config of initial number of seeds for given houses
@@ -27,13 +27,13 @@ class SolitaireMancala:
         """
         self.__config = list(__config)
         # print "__config - " + str(self.__config)
-    
+
     def __str__(self):
         """
         Return string representation for Mancala board
         """
         return str(self.__config[::-1])
-    
+
     def get_num_seeds(self, house_num):
         """
         Return the number of seeds in given house on board
@@ -42,7 +42,7 @@ class SolitaireMancala:
         # print "inside get_num_seeds..."
         # print "house_num - ", house_num
         # print "seeds - ", self.__config[house_num]
-        
+
         return self.__config[house_num]
 
     def is_game_won(self):
@@ -50,17 +50,17 @@ class SolitaireMancala:
         Check to see if all houses but house zero are empty
         """
         # print "inside is_game_won..."
-        
+
         for index in range(len(self.__config)-1):
             if self.__config[index+1] != 0:
                 # print str(self.__config)
                 # print "game lost!"
                 return False
-            
+
         # print "game won!"
         return True
-    
-    
+
+
     def is_legal_move(self, house_num):
         """
         Check whether a given move is legal
@@ -68,15 +68,15 @@ class SolitaireMancala:
         # print
         # print "inside is_legal_move..."
         # print "house_num - ", house_num,
-        
+
         if house_num == 0 or self.__config[house_num] != house_num:
             # print False
             return False
-    
+
         # print True
         return True
 
-    
+
     def apply_move(self, house_num):
         """
         Move all of the stones from house to lower/left houses
@@ -85,27 +85,27 @@ class SolitaireMancala:
         # print
         # print "inside apply_move..."
         # print "move chosen - ", house_num
-        
+
         # print "before move - "
         # print self.__config
-       
+
         if not self.is_legal_move(house_num):
             return
         if self.__config[house_num] == 0:
             return
-        
+
         self.__config[house_num] = 0
-        
+
         # print self.__config[house_num]
-        
+
         # print range(house_num, 0, -1)
         for index in range(house_num-1, 0, -1):
             self.__config[index] = self.__config[index] + 1
-        
+
         self.__config[0] = self.__config[0] + 1
         # print "after move - "
         # print self.__config
-            
+
     def choose_move(self):
         """
         Return the house for the next shortest legal move
@@ -116,24 +116,24 @@ class SolitaireMancala:
         # print
         # print "inside choose_move..."
         legal_moves = []
-        
+
         for index in range(len(self.__config)):
-            
+
             if index != 0:
                 if self.is_legal_move(index):
                     legal_moves.append(index)
-                    
+
         # print "legal_moves - ", legal_moves
         # print "move chosen - ", min(legal_moves)
         if legal_moves:
             return min(legal_moves)
         else:
             return 0
-    
+
     def plan_moves(self):
         """
-        Return a sequence (list) of legal moves based on the following heuristic: 
-        After each move, move the seeds in the house closest to the store 
+        Return a sequence (list) of legal moves based on the following heuristic:
+        After each move, move the seeds in the house closest to the store
         when given a choice of legal moves
         Not used in GUI version, only for machine testing
         """
@@ -141,25 +141,25 @@ class SolitaireMancala:
         # print "inside plan_moves..."
 
         backup = self.__config
-        
+
         print "current __config - ", self.__config
         ideal_moves = []
-        
+
         while(not self.is_game_won()):
             current_move = self.choose_move()
             # print
             # print "current_move - ", current_move
             if not current_move:
                 return ideal_moves
-            
+
             self.apply_move(current_move)
             ideal_moves.append(current_move)
 
         print "ideal moves - ", ideal_moves
-        
+
         self.__config = backup
         return ideal_moves
- 
+
 
 # Create tests to check the correctness of your code
 
@@ -167,14 +167,14 @@ def test_mancala():
     """
     Test code for Solitaire Mancala
     """
-    
+
     my_game = SolitaireMancala()
     # print "Testing init - Computed:", my_game, "Expected: [0]"
-    
-    __config1 = [0, 0, 1, 1, 3, 5, 0] 
+
+    __config1 = [0, 0, 1, 1, 3, 5, 0]
     #__config1 = [0, 0, 0, 0]
-    my_game.set_board(__config1)   
-    
+    my_game.set_board(__config1)
+
 #    # print "Testing set_board - Computed:", str(my_game), "Expected:", str([0, 5, 3, 1, 1, 0, 0])
 #    # print "Testing get_num_seeds - Computed:", my_game.get_num_seeds(1), "Expected:", __config1[1]
 #    # print "Testing get_num_seeds - Computed:", my_game.get_num_seeds(3), "Expected:", __config1[3]
@@ -183,13 +183,14 @@ def test_mancala():
     print "playing the game....."
     my_game.plan_moves()
     my_game.is_game_won()
-    
+
     # add more tests here
-    
+
 #test_mancala()
 
 
 # Import GUI code once you feel your code is correct
 #import poc_mancala_gui
 #poc_mancala_gui.run_gui(SolitaireMancala())
+
 
