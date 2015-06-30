c48ffd02417ed515a5e05482b0aee5cff6a2ef49
narayan <anuragallahabad@gmail.com>
2015-06-30 22:29:39 +0530

    greedy_boss- finishes basic implementation

--- cookieclicker/greedy_boss.py
+++ cookieclicker/greedy_boss.py
@@ -22,22 +22,42 @@ def greedy_boss(days_in_simulation, bribe_cost_increment, plot_type = STANDARD):
     """
 
     # initialize necessary local variables
+    savings = 0
+    current_day = 0
+    total_savings = 0
 
+    salary = INITIAL_SALARY
+    bribe_cost = INITIAL_BRIBE_COST
     # initialize list consisting of days vs. total salary earned for analysis
     days_vs_earnings = [(0, 0)]
 
     # Each iteration of this while loop simulates one bribe
+
+
     while current_day <= days_in_simulation:
 
         # check whether we have enough savings to bribe without waiting
+        count = 0
+
+        while savings < bribe_cost:
+            savings += salary
+            total_savings += salary
+            count += 1
+
+
 
         # advance current_day to day of next bribe (DO NOT INCREMENT BY ONE DAY)
+        current_day += count
 
         # update state of simulation to reflect bribe
+        savings -= bribe_cost
+        bribe_cost += bribe_cost_increment
+        salary += SALARY_INCREMENT
+
 
         # update list with days vs total salary earned for most recent bribe
         # use plot_type to control whether regular or log/log plot
-
+        days_vs_earnings.append((current_day,  total_savings))
 
 
     return days_vs_earnings
@@ -58,11 +78,11 @@ def run_simulations():
                          ["Bribe increment = 0", "Bribe increment = 500",
                           "Bribe increment = 1000", "Bribe increment = 2000"])
 
-run_simulations()
+#run_simulations()
 
 print greedy_boss(35, 100)
-# should print [(0, 0), (10, 1000), (16, 2200), (20, 3400), (23, 4600), (26, 6100), (29, 7900), (31, 9300), (33, 10900), (35, 12700), (37, 14700)]
+print '[(0, 0), (10, 1000), (16, 2200), (20, 3400), (23, 4600), (26, 6100), (29, 7900), (31, 9300), (33, 10900), (35, 12700), (37, 14700)]'
 
-print greedy_boss(35, 0)
+#print greedy_boss(35, 0)
 # should print [(0, 0), (10, 1000), (15, 2000), (19, 3200), (21, 4000), (23, 5000), (25, 6200), (27, 7600), (28, 8400), (29, 9300), (30, 10300), (31, 11400), (32, 12600), (33, 13900), (34, 15300), (34, 15300), (35, 16900), (36, 18600)]
 
