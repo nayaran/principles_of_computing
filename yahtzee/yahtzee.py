527f96a2e45e61cba6e445438855360733f08c6c
narayan <anuragallahabad@gmail.com>
2015-06-30 13:32:30 +0530

    yahtzee- finishes initial implementation, yet to test

--- yahtzee/yahtzee.py
+++ yahtzee/yahtzee.py
@@ -23,6 +23,7 @@ def gen_all_sequences(outcomes, length):
                 temp_set.add(tuple(new_sequence))
         answer_set = temp_set
     return answer_set
+
 def gen_hold_helper(outcomes, length):
     """
     Function that creates all permuations of the given length
@@ -58,6 +59,7 @@ def gen_hold_helper(outcomes, length):
 
 
     return ans
+
 def score(hand):
     """
     Compute the maximal score for a Yahtzee hand according to the
@@ -67,7 +69,11 @@ def score(hand):
 
     Returns an integer score
     """
-    print hand
+    print
+    print 'inside score......'
+    print 'hand- ', hand
+
+
     max_score = 0
     score_dict = {}
 
@@ -84,7 +90,7 @@ def score(hand):
     # Find the maximum score for the given hand
     max_score = max(score_dict.values())
 
-    print max_score
+    print 'max_score- ', max_score
     return max_score
 
 
@@ -99,8 +105,16 @@ def expected_value(held_dice, num_die_sides, num_free_dice):
 
     Returns a floating point expected value
     """
+    print
+    print 'inside expected_value......'
+    print 'held_dice- ', held_dice
+    print 'num_die_sides- ', num_die_sides
+    print 'num_free_dice- ', num_free_dice
+
 
     expected_value = 0.0
+
+    possible_expected_values = []
 
     # Generate [1, 2, 3, 4, 5, 6]
     outcomes = [dice_value for dice_value in range(1, 7)]
@@ -109,20 +123,26 @@ def expected_value(held_dice, num_die_sides, num_free_dice):
 
     # Generate all the possible outcomes
     all_sequences = gen_all_sequences(outcomes, length)
-    print all_sequences
+    #print all_sequences
 
     # Compute expected values of the generated outcomes
     for seq in all_sequences:
+        current_expected = 0.0
         for dice_value in seq:
-            expected_value += (dice_value * (1.0/6.0))
+            current_expected += (dice_value * (1.0/6.0))
 
-    print expected_value
+    possible_expected_values.append(current_expected)
+
+    for value in possible_expected_values:
+        expected_value += value
+
+    expected_value = expected_value/len(possible_expected_values)
 
     # Compute expected value for the held dices and generated outcomes
     for dice_value in held_dice:
-        expected_value += (dice_value * (1.0/6.0))
+        expected_value += (dice_value)
 
-    print expected_value
+    print 'expected_value- ', expected_value
 
     return expected_value
 
@@ -134,6 +154,11 @@ def gen_all_holds(hand):
 
     Returns a set of tuples, where each tuple is dice to hold
     """
+    print
+    print 'inside gen_all_holds......'
+    print 'hand- ', hand
+
+
     length = len(hand)
     possible_holds = set([()])
 
@@ -157,11 +182,13 @@ def gen_all_holds(hand):
     ans = temp2
 
     print 'total_holds- ', len(ans)
+    print 'holds- '
     print ans
 
     return ans
 
 def strategy(hand, num_die_sides):
+
     """
     Compute the hold that maximizes the expected value when the
     discarded dice are rolled.
@@ -172,36 +199,65 @@ def strategy(hand, num_die_sides):
     Returns a tuple where the first element is the expected score and
     the second element is a tuple of the dice to hold
     """
-    return (0.0, ())
+    print
+    print 'inside strategy......'
+    print 'hand- ', hand
+    print 'num_die_sides- ', num_die_sides,
+
+    all_possible_holds = gen_all_holds(hand)
+
+    best_hold = ()
+    max_expected = 0.0
+
+    for held_dice in all_possible_holds:
+
+        num_free_dice = len(hand) - len(held_dice)
+        current_expectation = expected_value(held_dice, num_die_sides, num_free_dice)
+        print
+        print 'with held_dice- ', held_dice
+        print 'expectation is- ', current_expectation
+
+        if current_expectation > max_expected:
+            max_expected = current_expectation
+            best_hold = held_dice
+
+
+    return (max_expected, best_hold)
+
+    #return (0.0, ())
 
 def run_example():
     """
     Compute the dice to hold and expected score for an example hand
     """
+    print
+    print 'playing yahtzee...........'
     num_die_sides = 6
-    hand = (1, 2, 1, 5, 6)
-    print
-    print 'score', hand
-    score(hand)
+    hand = (1, 1, 3, 4, 5)
+    #print
+    print 'hand', hand
+    #score(hand)
 
-    print
+    #print
 
-    held_dice = (1, 1)
-    num_free_dice = len(hand) - len(held_dice)
-    print 'expected_value(', held_dice,
-    print ',', num_die_sides,
-    print ',',num_free_dice,
-    print ')'
-    expected_value(held_dice, num_die_sides, num_free_dice)
+    #held_dice = (1, 1)
+    #num_free_dice = len(hand) - len(held_dice)
+    #print 'expected_value(', held_dice,
+    #print ',', num_die_sides,
+    #print ',',num_free_dice,
+    #print ')'
+    #expected_value(held_dice, num_die_sides, num_free_dice)
 
-    print
+    #print
     #hand = (1, 2, 1)
 
-    print 'gen_all_holds', hand, ''
-    gen_all_holds(hand)
+    #print 'gen_all_holds', hand, ''
+    #gen_all_holds(hand)
 
-#    hand_score, hold = strategy(hand, num_die_sides)
-#    print "Best strategy for hand", hand, "is to hold", hold, "with expected score", hand_score
+    hand_score, hold = strategy(hand, num_die_sides)
+    print
+    print
+    print "Best strategy for hand", hand, "is to hold", hold, "with expected score", hand_score
 
 run_example()
 
