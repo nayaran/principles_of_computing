e5c729cd59750c8146c56daeb6e6ed5e61120a00
narayan <anuragallahabad@gmail.com>
2015-07-02 12:36:28 +0530

    zombie- fixes queue implementation

--- zombie/queue.py
+++ zombie/queue.py
@@ -1,143 +1,52 @@
 """
-Grid class
+Queue class
 """
 
-EMPTY = 0
-FULL = 1
-
-class Grid:
+class Queue:
     """
-    Implementation of 2D grid of cells
-    Includes boundary handling
+    A simple implementation of a FIFO queue.
     """
 
-    def __init__(self, grid_height, grid_width):
+    def __init__(self):
         """
-        Initializes grid to be empty, take height and width of grid as parameters
-        Indexed by rows (left to right), then by columns (top to bottom)
+        Initialize the queue.
         """
-        self._grid_height = grid_height
-        self._grid_width = grid_width
-        self._cells = [[EMPTY for dummy_col in range(self._grid_width)]
-                       for dummy_row in range(self._grid_height)]
+        self._items = []
+
+    def __len__(self):
+        """
+        Return the number of items in the queue.
+        """
+        return len(self._items)
+
+    def __iter__(self):
+        """
+        Create an iterator for the queue.
+        """
+        for item in self._items:
+            yield item
 
     def __str__(self):
         """
-        Return multi-line string represenation for grid
+        Return a string representation of the queue.
         """
-        ans = ""
-        for row in range(self._grid_height):
-            ans += str(self._cells[row])
-            ans += "\n"
-        return ans
+        return str(self._items)
 
-    def get_grid_height(self):
+    def enqueue(self, item):
         """
-        Return the height of the grid for use in the GUI
+        Add item to the queue.
         """
-        return self._grid_height
+        self._items.append(item)
 
-    def get_grid_width(self):
+    def dequeue(self):
         """
-        Return the width of the grid for use in the GUI
+        Remove and return the least recently inserted item.
         """
-        return self._grid_width
-
+        return self._items.pop(0)
 
     def clear(self):
         """
-        Clears grid to be empty
+        Remove all items from the queue.
         """
-        self._cells = [[EMPTY for dummy_col in range(self._grid_width)]
-                       for dummy_row in range(self._grid_height)]
+        self._items = []
 
-    def set_empty(self, row, col):
-        """
-        Set cell with index (row, col) to be empty
-        """
-        self._cells[row][col] = EMPTY
-
-    def set_full(self, row, col):
-        """
-        Set cell with index (row, col) to be full
-        """
-        self._cells[row][col] = FULL
-
-    def is_empty(self, row, col):
-        """
-        Checks whether cell with index (row, col) is empty
-        """
-        return self._cells[row][col] == EMPTY
-
-    def four_neighbors(self, row, col):
-        """
-        Returns horiz/vert neighbors of cell (row, col)
-        """
-        ans = []
-        # NORTH
-        if row > 0:
-            ans.append((row - 1, col))
-
-        # SOUTH
-        if row < self._grid_height - 1:
-            ans.append((row + 1, col))
-
-        # WEST
-        if col > 0:
-            ans.append((row, col - 1))
-
-        # EAST
-        if col < self._grid_width - 1:
-            ans.append((row, col + 1))
-
-        return ans
-
-    def eight_neighbors(self, row, col):
-        """
-        Returns horiz/vert neighbors of cell (row, col) as well as
-        diagonal neighbors
-        """
-        ans = []
-
-        # NORTH
-        if row > 0:
-            ans.append((row - 1, col))
-
-        # SOUTH
-        if row < self._grid_height - 1:
-            ans.append((row + 1, col))
-
-        # WEST
-        if col > 0:
-            ans.append((row, col - 1))
-
-        # EAST
-        if col < self._grid_width - 1:
-            ans.append((row, col + 1))
-
-        # NORTH-WEST
-        if (row > 0) and (col > 0):
-            ans.append((row - 1, col - 1))
-
-        # NORTH-EAST
-        if (row > 0) and (col < self._grid_width - 1):
-            ans.append((row - 1, col + 1))
-
-        # SOUTH-WEST
-        if (row < self._grid_height - 1) and (col > 0):
-            ans.append((row + 1, col - 1))
-
-        # SOUTH-EAST
-        if (row < self._grid_height - 1) and (col < self._grid_width - 1):
-            ans.append((row + 1, col + 1))
-        return ans
-
-    def get_index(self, point, cell_size):
-        """
-        Takes point in screen coordinates and returns index of
-        containing cell
-        """
-        return (point[1] / cell_size, point[0] / cell_size)
-
-#new_grid = Grid(5, 10)
-#print new_grid
